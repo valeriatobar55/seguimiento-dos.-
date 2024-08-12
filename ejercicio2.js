331685fe8ceb28ba9f9a9a9af44d169c09a1db36
valeriatobar55 <valeriatobargarzon@gmail.com>
2024-08-12 18:35:03 -0500

    Update ejercicio2.js

---
+++ ejercicio2.js
@@ -0,0 +1,9 @@
+let numero = 5; 
+
+if (numero > 0) {
+    console.log("El número es positivo.");
+} else if (numero < 0) {
+    console.log("El número es negativo.");
+} else {
+    console.log("El número es cero.");
+}
