979527cc3e176534c4a971e29fef9914450786f4
valeriatobar55 <valeriatobargarzon@gmail.com>
2024-08-12 18:35:34 -0500

    ejercicio6.js

---
+++ ejercicio6.js
@@ -0,0 +1,11 @@
+let num1 = 5;
+let num2 = 12;
+let num3 = 7;
+
+if (num1 >= num2 && num1 >= num3) {
+    console.log("El número mayor es: " + num1);
+} else if (num2 >= num1 && num2 >= num3) {
+    console.log("El número mayor es: " + num2);
+} else {
+    console.log("El número mayor es: " + num3);
+}
