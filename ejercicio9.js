93201d7205e43595c571610f6c6e9be5eafcb458
valeriatobar55 <valeriatobargarzon@gmail.com>
2024-08-12 18:36:37 -0500

    Update ejercicio9.js

---
+++ ejercicio9.js
@@ -0,0 +1,7 @@
+let triangulo= prompt("nombra el triangulo(a, b, c)")
+    if (a + b + c === 180) {
+       
+        console.log("El triángulo es válido");
+    } else {
+        console.log("El triángulo no es válido");
+    }
